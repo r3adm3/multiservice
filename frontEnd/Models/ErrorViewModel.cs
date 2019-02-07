75a41b12e8c4c69842dc30fe75dc9a5de0c05072
Adrian Farnell <adrianjfarnell@gmail.com>
2019-02-07 20:38:41 +0000

    First commit, docker-compose works

---
+++ frontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace frontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
