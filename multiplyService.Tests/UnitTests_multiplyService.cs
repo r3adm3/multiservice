4a8c75b715c1a9b86450fafc8d4a7760b37c70f5
Adrian Farnell <adrianjfarnell@gmail.com>
2019-02-26 21:11:40 +0000

    multiply service unit test done

---
+++ multiplyService.Tests/UnitTests_multiplyService.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using multiplyService.Controllers;
+using Microsoft.Extensions.Configuration; 
+
+namespace multiplyService.Tests
+{
+    [TestClass]
+    public class unitTests_multiplyController
+    {
+        [TestMethod]
+        public void multiplyControllerTwoMultiplyOneEqualsTwo()
+        {
+            var controller = new multiplyController();
+
+            var result = controller.Get(2,1);
+
+            var actualResult = result.Value;
+
+            Assert.AreEqual(2, actualResult);
+
+        }
+
+    }
+}
