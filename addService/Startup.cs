243b6a5f7ab65f5b15e353eb99125b930dea31d0
Adrian Farnell <adrianjfarnell@gmail.com>
2019-09-24 21:14:56 +0100

    updated addService from .net core 2.2 to 3.0

--- addService/Startup.cs
+++ addService/Startup.cs
@@ -25,21 +25,14 @@ namespace addService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
             app.UseCors(
